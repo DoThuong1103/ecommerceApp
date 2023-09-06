319b4e722e164a9cb4146cdae77eb069df407035
DoThuong1103 <dothuong2k1@gmail.com>
2023-09-06 22:00:31 +0700

    final code
    ecommerce mern stack app

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
-
+import path from "path"
 //configure env
 dotenv.config();
 
@@ -17,13 +17,14 @@ const app = express();
 //middelwares
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
