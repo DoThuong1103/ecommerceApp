f4811c1b2a6b401a0a88aa2837e3c6e24d4b59e4
DoThuong1103 <dothuong2k1@gmail.com>
2023-09-06 22:15:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import path from "path"
+import {fileURLToPath} from 'url';
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+//esmodeule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
